989cb705d52467eee0b6fed20809accd5867711e
Cristiam Castellanos <64295092+CrisCastell@users.noreply.github.com>
2024-10-18 18:39:35 -0300

    Success register

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -1,9 +1,10 @@
-from django.db.models.signals import post_migrate
-from django.contrib.auth.models import Group
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User, Group
 
-@receiver(post_migrate)
-def create_client_group(sender, **kwargs):
-    group, created = Group.objects.get_or_create(name='clientes')
-    if created:
-        group.permissions.clear() 
+@receiver(post_save, sender=User)
+def add_user_to_client_group(sender, instance, created, **kwargs):
+    # Si el usuario es nuevo y no es un superusuario, lo agregamos al grupo "clientes"
+    if created and not instance.is_superuser:
+        client_group, created = Group.objects.get_or_create(name='clientes')
+        instance.groups.add(client_group)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,49 +1,17 @@
-from django.contrib.auth.models import User, Group, Permission
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
-
-
+from .forms import CustomUserCreationForm
 
 @user_passes_test(lambda u: not u.is_authenticated, login_url='/')
 def registro_cliente(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.is_staff = True
-            user.save()
-
-            # Si el usuario no es superusuario, agregarlo al grupo "clientes"
-            if not user.is_superuser:
-                client_group = Group.objects.get(name='clientes')
-                user.groups.add(client_group)
-
-            messages.success(request, f'Usuario {user.username} registrado exitosamente.')
+            form.save()
+            messages.success(request, 'Usuario registrado exitosamente.')
             return redirect('admin:index')
     else:
-        form = UserCreationForm()
-
+        form = CustomUserCreationForm()
+    
     return render(request, 'admin/register.html', {'form': form})
-
-
-# def registro_cliente(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             user.is_staff = True
-#             user.save()
-
-#             client_group = Group.objects.get(name='clientes')
-#             user.groups.add(client_group)
-
-#             messages.success(request, f'Usuario {user.username} registrado exitosamente como cliente.')
-#             return redirect('admin:index')  
-#     else:
-#         form = UserCreationForm()
-#         print(form)
-
-#     return render(request, 'admin/register.html', {'form': form})
